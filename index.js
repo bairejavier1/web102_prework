d6c97a4d6d740773d70c0a3f3506488a456a0df1
bairejavier1 <bairejavier1@gmail.com>
2025-05-02 23:42:11 -0400

    Add summary statistics for contributions, raised amount, and number of games

--- index.js
+++ index.js
@@ -70,20 +70,26 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce((total, game) => total + game.backers, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${totalContributions.toLocaleString()} contributions`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+const totalRaised = GAMES_JSON.reduce((total, game) => total + game.pledged, 0);
 
 // set inner HTML using template literal
-
+raisedCard.innerHTML = `$${totalRaised.toLocaleString()}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
 
+// count the number of games
+const numGames = GAMES_JSON.length;
+
+// set the number of games
+gamesCard.innerHTML = `${numGames} games`;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
